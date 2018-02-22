3a62a83997faadf35642665d26686358272e34a4
Sébastien Lerique <sl@mehho.net>
2018-02-22 09:17:32 -0300

    docs/rust: explain current state of the extension

--- rust-utils/src/lib.rs
+++ rust-utils/src/lib.rs
@@ -13,6 +13,17 @@ fn split(string: &str, sep: &Regex) -> Vec<String> {
 
 fn array_split(strings: &[String], sep: &str, parallel: bool)
         -> Result<Vec<Vec<String>>, regex::Error> {
+    // This has the (important) downside that it must generate new
+    // String objects for each of the split parts.
+    // To get around this, we might want to convert to embeddings directly
+    // in here, or return slices of the parts by pointing to the original data,
+    // or investigate how to let python do that by giving it a list of slices
+    // directly.
+    //
+    // Of interest:
+    // - https://github.com/bluss/rust-ndarray/issues/137
+    // - https://github.com/termoshtt/rust-numpy/issues/23
+
     let sep = Regex::new(sep)?;
     let splits = if parallel {
         strings.par_iter().map(|string| split(string, &sep)).collect()
