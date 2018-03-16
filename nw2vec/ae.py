ac768fc4fa11713379d7a636a832362c7a1148e2
Sébastien Lerique <sl@mehho.net>
2018-03-16 16:49:27 +0100

    feat/ae: first conceptually complete version
    Now, we need to test and fix all the errors :)

--- nw2vec/ae.py
+++ nw2vec/ae.py
@@ -3,66 +3,101 @@ from keras import backend as K
 
 from nw2vec import layers
 from nw2vec import utils
+from nw2vec import codecs
 
 
-# Set up the VAE
-
-dim_data = 40    # Dimension base features for each node
-dim_l1 = 20   # Dimension of intermediate layers
-dim_z = 5    # Dimension of the embedding space
-n_ξ_samples = 10
-adj = []  # Adjacency matrix for convolutions
-
-# Encoder tensor
-q_input = keras.layers.Input(shape=(dim_data,), name='q_input')
-# CANDO: change act
-q_layer1 = layers.GC(dim_l1, adj, use_bias=True, act='relu', name='q_layer1')(q_input)
-q_μ_flat = layers.GC(dim_z, adj, use_bias=True, name='q_μ_flat')(q_layer1)
-q_logD_flat = layers.GC(dim_z, adj, use_bias=True, name='q_logD_flat')(q_layer1)
-q_u_flat = layers.GC(dim_z, adj, use_bias=True, name='q_u_flat')(q_layer1)
-q_μlogDu_flat = keras.layers.Concatenation(name='q_μlogDu_flat')([q_μ_flat, q_logD_flat, q_u_flat])
-q_model = keras.models.Model(inputs=q_input, outputs=q_μlogDu_flat)
-
-# Decoder model
-p_input = keras.layers.Input(shape=(dim_z,), name='p_input')
-# CANDO: change act
-p_layer1 = keras.layers.Dense(dim_l1, use_bias=True, act='relu', name='p_layer1')(p_input)
-p_adj = layers.Bilinear(adj.shape[0], name='p_adj')(p_layer1)
-p_v_μ_flat = keras.layers.Dense(dim_data, use_bias=True, name='p_v_μ_flat')(p_layer1)
-p_v_logD_flat = keras.layers.Dense(dim_data, use_bias=True, name='p_v_logD_flat')(p_layer1)
-p_v_u_flat = keras.layers.Dense(dim_data, use_bias=True, name='p_v_u_flat')(p_layer1)
-p_v_μlogDu_flat = keras.layers.Concatenation(name='p_v_μlogDu_flat')(
-    [p_v_μ_flat, p_v_logD_flat, p_v_u_flat])
-p_model = keras.models.Model(inputs=p_input, outputs=[p_adj, p_v_μlogDu_flat])
+# Example values for the parameters
+# dim_data = 40    # Dimension base features for each node
+# dim_l1 = 20      # Dimension of intermediate layers
+# dim_z = 5        # Dimension of the embedding space
+# n_ξ_samples = 10
 
 
-# ADD
-# - encoder KL to normal
-# - decoder regularizer ||θ||**2 / (2 * κ)
+def build_q_model(adj, dims):
+    dim_data, dim_l1, dim_z = dims
+
+    q_input = keras.layers.Input(shape=(dim_data,), name='q_input')
+    # CANDO: change act
+    # q_layer1 = layers.GC(dim_l1, adj, use_bias=True, act='relu', name='q_layer1')(q_input)
+    # q_μ_flat = layers.GC(dim_z, adj, use_bias=True, name='q_μ_flat')(q_layer1)
+    # q_logD_flat = layers.GC(dim_z, adj, use_bias=True, name='q_logD_flat')(q_layer1)
+    # q_u_flat = layers.GC(dim_z, adj, use_bias=True, name='q_u_flat')(q_layer1)
+    q_layer1 = keras.layers.Dense(dim_l1, adj, use_bias=True, act='relu',
+                                  name='q_layer1')(q_input)
+    q_μ_flat = keras.layers.Dense(dim_z, adj, use_bias=True, name='q_μ_flat')(q_layer1)
+    q_logD_flat = keras.layers.Dense(dim_z, adj, use_bias=True, name='q_logD_flat')(q_layer1)
+    q_u_flat = keras.layers.Dense(dim_z, adj, use_bias=True, name='q_u_flat')(q_layer1)
+    q_μlogDu_flat = keras.layers.Concatenation(name='q_μlogDu_flat')(
+        [q_μ_flat, q_logD_flat, q_u_flat])
+    q_model = keras.models.Model(inputs=q_input, outputs=q_μlogDu_flat)
+
+    return q_input, q_model, codecs.Gaussian
 
 
-def vae(q_input, q_model, q_codec, n_ξ_samples, p_model, p_codecs):
-    ξ = layers.ParametrisedStochastic(q_codec, n_ξ_samples)(q_model(q_input))
-    model = keras.models.Model(inputs=q_input, outputs=p_model(ξ))
+def build_p_model(dims):
+    dim_data, dim_l1, dim_z = dims
 
-    def estimated_codec_loss(codec, y_true, y_pred):
+    p_input = keras.layers.Input(shape=(dim_z,), name='p_input')
+    # CANDO: change act
+    p_layer1 = keras.layers.Dense(dim_l1, use_bias=True, act='relu',
+                                  kernel_regularizer='l2', bias_regularizer='l2',
+                                  name='p_layer1')(p_input)
+    p_adj = layers.Bilinear(0, use_bias=False, act='sigmoid',
+                            kernel_regularizer='l2', bias_regularizer='l2',
+                            name='p_adj')(p_layer1)
+    p_v_μ_flat = keras.layers.Dense(dim_data, use_bias=True,
+                                    kernel_regularizer='l2', bias_regularizer='l2',
+                                    name='p_v_μ_flat')(p_layer1)
+    p_v_logD_flat = keras.layers.Dense(dim_data, use_bias=True,
+                                       kernel_regularizer='l2', bias_regularizer='l2',
+                                       name='p_v_logD_flat')(p_layer1)
+    p_v_u_flat = keras.layers.Dense(dim_data, use_bias=True,
+                                    kernel_regularizer='l2', bias_regularizer='l2',
+                                    name='p_v_u_flat')(p_layer1)
+    p_v_μlogDu_flat = keras.layers.Concatenation(name='p_v_μlogDu_flat')(
+        [p_v_μ_flat, p_v_logD_flat, p_v_u_flat])
+    p_model = keras.models.Model(inputs=p_input, outputs=[p_adj, p_v_μlogDu_flat])
+
+    return p_input, p_model, (codecs.Bernoulli, codecs.Gaussian)
+
+
+def vae(adj, q_input, q_model, q_codec, n_ξ_samples, p_model, p_codecs):
+    # Wire up the model
+    q = q_model(q_input)
+    ξ = layers.ParametrisedStochastic(q_codec, n_ξ_samples)(q)
+    model = keras.models.Model(inputs=q_input, outputs=[q] + p_model(ξ))
+
+    # Define the losses
+    def q_loss(y_true, y_pred):
+        return codecs.get(q_codec, y_pred).kl_to_normal()
+
+    p_adj_codec, p_v_codec = p_codecs
+
+    def estimated_p_adj_loss(y_true, y_pred):
+        batch_size = y_true.shape[0]
+        assert adj.shape[0] == batch_size
+        assert y_pred.shape == (n_ξ_samples, batch_size)
+        y_pred_flat = K.reshape(y_pred, (n_ξ_samples, batch_size ** 2))
+        sample_adj_flat = utils.expand_dims_tile(adj.flatten(), 0, n_ξ_samples)
+        global_logprob = K.mean(codecs.get(p_adj_codec, y_pred_flat)
+                                .logprobability(sample_adj_flat),
+                                axis=-2)
+        return - utils.expand_dims_tile(global_logprob, 0, batch_size)
+
+    def estimated_p_v_loss(y_true, y_pred):
         y_true = utils.expand_dims_tile(y_true, -2, y_pred.shape[-2])
-        return K.mean(codec(y_pred).logprobability(y_true), axis=-2)
+        return - K.mean(codecs.get(p_v_codec, y_pred).logprobability(y_true), axis=-2)
 
+    # Compile the whole thing with losses
     model.compile('adam',  # CANDO: tune parameters
-                  losses=[estimated_codec_loss(codec) for codec in p_codecs],
-                  # TODO: loss_weights
+                  losses=[q_loss, estimated_p_adj_loss, estimated_p_v_loss],
+                  loss_weights=[1.0, 1.0, 1.0],  # TODO: tune loss_weights
                   # TODO: metrics
                   )
     return model
 
 
-# decoders could be made of decoder_adj and decoder_features.
-# decoder_adj should be Bernoulli, decoder_features should be
-# Gaussian, and they could also share a first layer. They can be
-# bilinear or MLP
-
-# encoders are further regularised (Rezende et al. 2014, p.10):
+# Note: encoders are further regularised (Rezende et al. 2014, p.10):
 #
 # > We regularise the recognition model by introducing
 # > additional noise, specifically, bit-flip or drop-out noise
@@ -74,19 +109,11 @@ def vae(q_input, q_model, q_codec, n_ξ_samples, p_model, p_codecs):
 # > recognition model is unable to provide accurate inferences
 # > for unseen data points.
 
-# QUESTIONS:
-#
-# - what obective to use, and how does it depend on
-# encoder/decoder
-#
+# TODO:
 # - we should weigh each contribution of decoders to the overall
-# loss according to its importance / number of contributing
-# items
-#
-# - how much help does TF need for gradient descent
-#
-# - how do we minibatch (knowing that it will be model-specific
-# because of convolution and the like)
-#
+#   loss according to its importance / number of contributing
+#   items
+# - minibatch (knowing that it will be model-specific
+#   because of convolution and the like)
 # - can the encoders incorporate side-features if the core
-# features are not distinctive enough?
+#   features are not distinctive enough?
--- nw2vec/layers.py
+++ nw2vec/layers.py
@@ -1,28 +1,144 @@
 import tensorflow as tf
 import keras
 from keras import backend as K
-from keras.engine.topology import _to_snake_case
+
+from nw2vec import codecs
 
 
 class GC(keras.layers.Layer):
-    pass
+
+    def __init__(self, units, adj):
+        # TODO
+        pass
 
 
-class Bilinear(keras.layers.Dot):
+class Bilinear(keras.layers.Layer):
 
-    def __init__(self, tensor1, tensor2, **kwargs):
-        assert tensor1.shape == tensor2.shape
+    def __init__(self,
+                 bilin_axes,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
 
-        name = kwargs.get('name')
-        if not name:
-            prefix = self.__class__.__name__
-            name = _to_snake_case(prefix) + '_' + str(K.get_uid(prefix))
-        self.name = name
+        if isinstance(bilin_axes, int):
+            bilin_axes = [bilin_axes]
+        assert isinstance(bilin_axes, list)
+        assert len(bilin_axes) >= 1
 
-        with tf.variable_scope(name):
-            self.transform = keras.layers.Dense(tensor1.shape[-1], use_bias=False,
-                                                name='transform')
-            super(Bilinear, self).__init__(self.transform(tensor1), tensor2, **kwargs)
+        super(Bilinear, self).__init__(**kwargs)
+        self.bilin_axes = bilin_axes
+        self.activation = keras.activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = keras.initializers.get(kernel_initializer)
+        self.bias_initializer = keras.initializers.get(bias_initializer)
+        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
+        self.bias_regularizer = keras.regularizers.get(bias_regularizer)
+        self.activity_regularizer = keras.regularizers.get(activity_regularizer)
+        self.kernel_constraint = keras.constraints.get(kernel_constraint)
+        self.bias_constraint = keras.constraints.get(bias_constraint)
+        self.input_spec = [keras.engine.InputSpec(min_ndim=2)] * 2
+        self.supports_masking = True
+
+    def _process_input_shapes(self, input_shapes):
+        if not isinstance(input_shapes, list) or len(input_shapes) != 2:
+            raise ValueError('A `Bilinear` layer should be called '
+                             'on a list of 2 inputs.')
+        # The two tensors must have the same shape
+        assert input_shapes[0] == input_shapes[1]
+        shape = input_shapes[0]
+        # Concretise bilin_axes for this shape
+        bilin_axes = [dim % len(shape) for dim in self.bilin_axes]
+        # Reduction axis cannot be in bilin_axes
+        assert (len(shape) - 1) not in bilin_axes
+        assert -1 not in bilin_axes
+        # Shape must be long enough to accomodate bilin_axes + reduction axis (=input_dim)
+        assert len(shape) >= len(bilin_axes) + 1
+
+        diag_axes = list(sorted(set(range(len(shape) - 1)).difference(bilin_axes)))
+        return bilin_axes, diag_axes, shape[-1]
+
+    def build(self, input_shapes):
+        _, _, input_dim = self._process_input_shapes(input_shapes)
+
+        self.kernel = self.add_weight(shape=(input_dim, input_dim),
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+
+        self.input_spec = [keras.layers.InputSpec(min_ndim=2, axes={-1: input_dim})] * 2
+        super(Bilinear, self).build(input_shapes)
+
+    def call(self, inputs):
+        tensor0, tensor1 = inputs
+        bilin_axes, diag_axes, _ = self._process_input_shapes([tensor0.shape, tensor1.shape])
+
+        Q_tensor0 = tf.tensordot(self.kernel, tensor0, axes=[[1], [-1]])
+        output = tf.tensordot(tensor1, Q_tensor0, axes=[[-1], [-1]])
+        # Put the bilinear axes first (in the order requested) and the diagonal axes last,
+        # for each half of the tensordot axes
+        n_bilin = len(bilin_axes)
+        n_diag = len(diag_axes)
+        output = tf.transpose(output,
+                              perm=(bilin_axes + diag_axes
+                                    + [ax + n_bilin + n_diag for ax in bilin_axes]
+                                    + [ax + n_bilin + n_diag for ax in diag_axes]))
+        # Take the diagonal elements for all the non-bilinear axis couples
+        perm_base = list(range(n_bilin)) + list(range(n_bilin + 1, n_bilin + n_diag))
+        for rem in range(n_bilin + n_diag - 1, n_bilin - 1, -1):
+            perm = (perm_base[:rem]
+                    + [rem + 1 + ax for ax in perm_base[:rem]]
+                    + [n_bilin, rem + 1 + n_bilin])
+            output = tf.transpose(output, perm=perm)
+            assert output.shape[-1] == output.shape[-2]
+            output = tf.matrix_diag_part(output)
+        # Put the diagonal axes first, the bilinear axes last
+        output = tf.transpose(output,
+                              perm=(list(range(2 * n_bilin, 2 * n_bilin + n_diag))
+                                    + list(range(n_bilin * 2))))
+
+        if self.use_bias:
+            output = K.bias_add(output, self.bias)
+        if self.activation is not None:
+            output = self.activation(output)
+        return output
+
+    def compute_output_shape(self, input_shapes):
+        bilin_axes, diag_axes, _ = self._process_input_shapes(input_shapes)
+        return tuple([input_shapes[0][ax] for ax in diag_axes + 2 * bilin_axes])
+
+    def get_config(self):
+        config = {
+            'bilin_axes': self.bilin_axes,
+            'activation': keras.activations.serialize(self.activation),
+            'use_bias': self.use_bias,
+            'kernel_initializer': keras.initializers.serialize(self.kernel_initializer),
+            'bias_initializer': keras.initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': keras.regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': keras.regularizers.serialize(self.bias_regularizer),
+            'activity_regularizer': keras.regularizers.serialize(self.activity_regularizer),
+            'kernel_constraint': keras.constraints.serialize(self.kernel_constraint),
+            'bias_constraint': keras.constraints.serialize(self.bias_constraint)
+        }
+        base_config = super(Bilinear, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
 
 
 class ParametrisedStochastic(keras.layers.Lambda):
@@ -31,6 +147,6 @@ class ParametrisedStochastic(keras.layers.Lambda):
         self.codec = codec
 
         def sampler(params):
-            return codec(params).stochastic_value(n_samples)
+            return codecs.get(codec, params).stochastic_value(n_samples)
 
         super(ParametrisedStochastic, self).__init__(sampler, **kwargs)
