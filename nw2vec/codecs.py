fix/codecs: make broadcast_left work also with statically unkwown shapes
Former-commit-id: a95f10e994a9ab0645bce14f78b942e5aa9af7f9

nw2vec/codecs.py
@@ -74,8 +74,8 @@ class Gaussian(Codec):
     # TOTEST
     def stochastic_value(self, n_samples):
         """TODOC"""
-        μ_shape = self.μ.shape.as_list()
-        ε = tf.random_normal(μ_shape[:-2] + [n_samples] + μ_shape[-2:])
+        μ_shape = tf.shape(self.μ)
+        ε = tf.random_normal(tf.concat([μ_shape[:-2], [n_samples], μ_shape[-2:]], 0))
         return K.squeeze(expand_dims_tile(self.μ, -3, n_samples)
                          + expand_dims_tile(self.R, -3, n_samples) @ ε,
                          -1)
@@ -86,7 +86,7 @@ class Gaussian(Codec):
         # Turn `v` into a column vector
         v = K.expand_dims(v, -1)
         # Check shapes and broadcast
-        v = broadcast_left(v, self.μ.shape)
+        v = broadcast_left(v, self.μ)
         return - .5 * (self.dim * np.log(2 * np.pi) + self.logdetC
                        + right_squeeze2(tf.matrix_transpose(v - self.μ)
                                         @ self.C_inv
@@ -111,7 +111,7 @@ class SigmoidBernoulli(Codec):
     def logprobability(self, v):
         """TODOC"""
         # Check shapes and broadcast
-        v = broadcast_left(v, self.logits.shape)
+        v = broadcast_left(v, self.logits)
         return - K.sum(tf.nn.sigmoid_cross_entropy_with_logits(labels=v, logits=self.logits),
                        axis=-1)
 
@@ -127,7 +127,7 @@ class Bernoulli(Codec):
     def logprobability(self, v):
         """TODOC"""
         # Check shapes and broadcast
-        v = broadcast_left(v, self.logits.shape)
+        v = broadcast_left(v, self.logits)
         return K.sum(v * K.log(self.probs) + (1.0 - v) * K.log(1 - self.probs), axis=-1)
 
 

nw2vec/utils.py
@@ -44,31 +44,27 @@ def Softmax(axes=[-1]):
 
 
 # TOTEST
-def broadcast_left(array, target_shape):
+def broadcast_left(array, target_array):
     """TODOC"""
 
     with tf.name_scope('broadcast') as scope:
         array = tf.convert_to_tensor(array)
-        if not isinstance(target_shape, tf.Tensor):
-            orig_target_shape = target_shape
-            target_shape = tf.convert_to_tensor(target_shape)
-        else:
-            orig_target_shape = None
         array_shape = tf.shape(array)
         array_rank = tf.rank(array)
-        target_rank = tf.shape(target_shape)[0]
+        target_array = tf.convert_to_tensor(target_array)
+        target_shape = tf.shape(target_array)
+        target_rank = tf.rank(target_array)
 
         with tf.control_dependencies(
-                [tf.assert_less_equal(array_rank, target_rank),
+                [tf.assert_rank_at_least(target_array, array_rank),
                  tf.assert_equal(array_shape, target_shape[- array_rank:])]):
             # Actually reshape and broadcast
             degen_shape = tf.concat([tf.ones(target_rank - array_rank, dtype=tf.int32), array_shape], 0)
             multiples = tf.concat([target_shape[:- array_rank], tf.ones(array_rank, dtype=tf.int32)], 0)
             out = tf.tile(tf.reshape(array, degen_shape), multiples, name=scope)
 
-            # Inform the static shape if possible
-            if orig_target_shape is not None:
-                out.set_shape(orig_target_shape)
+            # Inform the static shape
+            out.set_shape(target_array.shape)
 
             return out
 