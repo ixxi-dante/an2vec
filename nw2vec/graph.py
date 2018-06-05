1ada9c2aa7d538ecb2829c73889af315c2b8d30c
Sébastien Lerique <sl@mehho.net>
2018-06-05 14:02:16 +0200

    feat/graph: prepare for p/q seed-span sampling
    * make CSGraph immutable, i.e. we can't remove nodes
    * make it able to sample from neighbours following p/q depth-breadth
      parameters, with precomputation of an effecient "alias" method for
      sampling
    * update tests
    
    Former-commit-id: 0c7a1575de37290f208a53e31ceaf5aa920e0185

--- nw2vec/graph.py
+++ nw2vec/graph.py
@@ -3,6 +3,8 @@ from scipy import sparse
 import joblib
 from cached_property import cached_property
 
+from nw2vec import utils
+
 
 def csr_eye(n):
     return sparse.csr_matrix((np.ones(n), (np.arange(n), np.arange(n))), shape=(n, n))
@@ -10,9 +12,7 @@ def csr_eye(n):
 
 class CSGraph:
 
-    _REMOVED_LABEL = np.iinfo(np.int).max
-
-    def __init__(self, adj):
+    def __init__(self, adj, p, q):
         # Check the adjacency matrix is:
         # ... a sparse matrix...
         if not isinstance(adj, sparse.csr_matrix):
@@ -24,89 +24,75 @@ class CSGraph:
         # ... with no diagonal elements.
         assert adj.multiply(csr_eye(adj.shape[0])).sum() == 0
 
-        self._adj = adj.copy()
-        self._adj.eliminate_zeros()
-        self._initial_shape = self._adj.shape
-        self._ids2labels = np.arange(adj.shape[0])
-        self._labels2ids = np.arange(adj.shape[0])
-        self._kept_labels_bool = np.ones(adj.shape[0], dtype=bool)
+        self.adj = adj.copy()
+        self.adj.eliminate_zeros()
+        self._p = p
+        self._q = q
+        self._init_transition_probs()
 
-    def labels_exist(self, labels):
-        if isinstance(labels, set):
-            labels = np.array(list(labels))
-        elif isinstance(labels, (list, range)):
-            labels = np.array(labels)
-        elif not isinstance(labels, np.ndarray):
-            raise ValueError('Unsupported label type')
+    def _get_alias_edge(self, src, dst):
+        src_neigbours = self.neighbors(src)
+        dst_neigbours = self.neighbors(dst)
 
-        if len(labels) == 0:
-            return np.array([])
+        n_dst_neigbours = len(dst_neigbours)
+        assert n_dst_neigbours > 0
 
-        if ((labels < 0) + (labels >= self._initial_shape[0])).any():
-            raise IndexError
+        uprobs = np.zeros(n_dst_neigbours)
+        for i, next_dst in enumerate(dst_neigbours):
+            if next_dst == src:
+                uprobs[i] = 1.0 / self._p
+            elif next_dst in src_neigbours:
+                uprobs[i] = 1.0
+            else:
+                uprobs[i] = 1.0 / self._q
 
-        return self._labels2ids[labels] != self._REMOVED_LABEL
+        return utils.alias_setup(uprobs / uprobs.sum())
 
-    def remove_nodes_from(self, labels):
-        # Process labels
-        labels = list(labels)
-        assert self.labels_exist(labels).all()
+    def _init_transition_probs(self):
+        # Note: this first part will only be relevant once (if) we add edge weights
+        # (for now it's the same as uniform sampling)
+        # Create alias arrays for nodes without looking at incoming link
+        # data_length = len(self.adj.data)
+        # data_dtype = self.adj.data.dtype
+        # node_alias_choices = np.zeros(data_length, dtype=np.int)
+        # node_alias_weights = np.zeros(data_length, dtype=data_dtype)
+        # for node in self.nodes:
+        #     ptrs = slice(self.adj.indptr[node], self.adj.indptr[node + 1])
+        #     uprobs = self.adj.data[ptrs]
+        #     node_alias_choices[ptrs], node_alias_weights[ptrs] = \
+        #         utils.alias_setup(uprobs / uprobs.sum())
+        #
+        # self.node_alias_choices = sparse.csr_matrix(
+        #     (node_alias_choices, self.adj.indices, self.adj.indptr), shape=self.adj.shape)
+        # self.node_alias_weights = sparse.csr_matrix(
+        #     (node_alias_weights, self.adj.indices, self.adj.indptr), shape=self.adj.shape)
 
-        # Pre-compute the shift of ids due to node removals
-        shifts = np.zeros_like(self._ids2labels, dtype=int)
-        shifts[self._labels2ids[labels]] = 1
-        shifts = shifts.cumsum()
+        # Create alias arrays for edges by looking at incoming link
+        edge_aliases = {}
+        for src_node in self.nodes:
+            for dst_node in self.neighbors(src_node):
+                edge_aliases[(src_node, dst_node)] = self._get_alias_edge(src_node, dst_node)
+        self._edge_aliases = edge_aliases
 
-        # Update our tracking of which labels are kept, get the corresponding ids,
-        # and reduce `shifts` to only tho kept ids
-        self._kept_labels_bool[labels] = False
-        kept_ids = self._labels2ids[self._kept_labels_bool]
-        shifts = shifts[kept_ids]
+    def draw_after_edge(self, src, dst):
+        drawn = utils.alias_draw(*self._edge_aliases[(src, dst)])
+        return self.neighbors(dst)[drawn]
 
-        # `labels2ids` receives the shifts for labels that are kept,
-        # and removed labels are marked as such
-        self._labels2ids[self._kept_labels_bool] -= shifts
-        self._labels2ids[labels] = self._REMOVED_LABEL
-
-        # Reduce ids2labels
-        self._ids2labels = self._ids2labels[kept_ids]
-
-        # Reduce the adjacency matrix
-        self._adj = self._adj[kept_ids, :][:, kept_ids]
-
-        # Invalidate cached properties
-        if 'nodes' in self.__dict__:
-            del self.__dict__['nodes']
-        if 'adj' in self.__dict__:
-            del self.__dict__['adj']
-
-    def neighbors(self, label):
-        assert label >= 0 and label < self._initial_shape[0]
-        idx = self._labels2ids[label]
-        neighbor_ids = self._adj.indices[self._adj.indptr[idx]:self._adj.indptr[idx + 1]]
-        return self._ids2labels[neighbor_ids]
+    def neighbors(self, idx):
+        assert idx >= 0 and idx < self.adj.shape[0]
+        return self.adj.indices[self.adj.indptr[idx]:self.adj.indptr[idx + 1]]
 
     @cached_property
     def nodes(self):
-        return self._ids2labels
+        return np.arange(self.adj.shape[0])
 
     @cached_property
-    def adj(self):
-        return self._adj
-
-    def copy(self):
-        other = CSGraph(self._adj)
-        other._ids2labels = self._ids2labels.copy()
-        other._labels2ids = self._labels2ids.copy()
-        other._kept_labels_bool = self._kept_labels_bool.copy()
-        return other
-
     def __key(self):
-        key = (self._adj, self._ids2labels, self._labels2ids)
+        key = (self.adj, self._p, self._q)
         return tuple(map(joblib.hash, key))
 
     def __hash__(self):
-        return hash(self.__key())
+        return hash(self.__key)
 
     def __eq__(self, other):
-        return self.__key() == other.__key()
+        return self.__key == other.__key
