920b425fd08ace396de67195b6186d16ac62595c
Sébastien Lerique <sl@mehho.net>
2018-06-05 12:30:17 +0200

    feat/utils: add alias sampling helpers and tests
    Former-commit-id: fb8b27bd18b999fc9dd933c946e681e46d7be126

--- nw2vec/utils_test.py
+++ nw2vec/utils_test.py
@@ -1,6 +1,50 @@
 import pytest
+import numpy as np
+from numpy.testing import assert_array_equal, assert_allclose
+import numba
 
 from nw2vec import utils
+
+
+def test_alias_setup():
+    # Bails if `probs` is not an ndarray
+    with pytest.raises(numba.errors.TypingError):
+        utils.alias_setup([.5, .5])
+
+    # Works with good arguments
+    probs = np.ones(4)
+    probs = probs / probs.sum()
+    choices, weights = utils.alias_setup(probs)
+    assert_array_equal(choices, [0, 0, 0, 0])
+    assert_array_equal(weights, [1, 1, 1, 1])
+
+    probs = np.array([1, 1, 2, 2])
+    probs = probs / probs.sum()
+    choices, weights = utils.alias_setup(probs)
+    assert_array_equal(choices, [3, 3, 0, 2])
+    assert_allclose(weights, [2 / 3, 2 / 3, 1, 2 / 3], rtol=1e-6)
+
+    probs = np.arange(5)
+    probs = probs / probs.sum()
+    choices, weights = utils.alias_setup(probs)
+    assert_array_equal(choices, [4, 4, 0, 0, 3])
+    assert_array_equal(weights, [0, .5, 1, 1, .5])
+
+
+def test_alias_draw():
+    # Works with good arguments
+    probs = np.arange(5)
+    probs = probs / probs.sum()
+    choices, weights = utils.alias_setup(probs)
+    assert utils.alias_draw(choices, weights) in range(1, 5)
+
+    # Gives the right statistics
+    n_draws = 1000
+    draws = np.array([utils.alias_draw(choices, weights) for _ in range(n_draws)])
+    counts = [(draws == i).sum() for i in range(5)]
+    assert counts[0] == 0
+    assert np.sum(counts[1:]) == n_draws
+    assert_allclose(counts[1:], n_draws * probs[1:], atol=50)
 
 
 @pytest.mark.skip(reason='Implement test')
