7d0727f0e0ea7af21892b3aa1e82ea33e61f868a
Sébastien Lerique <sl@mehho.net>
2018-06-08 16:54:40 +0200

    feat/ae: add a ModelBatchCheckpoint

--- nw2vec/ae.py
+++ nw2vec/ae.py
@@ -9,9 +9,111 @@ from keras.utils.data_utils import Sequence
 from keras.utils.data_utils import GeneratorEnqueuer
 from keras.utils.data_utils import OrderedEnqueuer
 from keras import callbacks as cbks
+import numpy as np
 
 from nw2vec import layers
 from nw2vec import codecs
+
+
+class ModelBatchCheckpoint(cbks.Callback):
+    """Save the model after every batch.
+    `filepath` can contain named formatting options,
+    which will be filled the value of `batch` and
+    keys in `logs` (passed in `on_batch_end`).
+    For example: if `filepath` is `weights.{batch:02d}-{val_loss:.2f}.hdf5`,
+    then the model checkpoints will be saved with the batch number and
+    the validation loss in the filename.
+    # Arguments
+        filepath: string, path to save the model file.
+        monitor: quantity to monitor.
+        verbose: verbosity mode, 0 or 1.
+        save_best_only: if `save_best_only=True`,
+            the latest best model according to
+            the quantity monitored will not be overwritten.
+        mode: one of {auto, min, max}.
+            If `save_best_only=True`, the decision
+            to overwrite the current save file is made
+            based on either the maximization or the
+            minimization of the monitored quantity. For `val_acc`,
+            this should be `max`, for `val_loss` this should
+            be `min`, etc. In `auto` mode, the direction is
+            automatically inferred from the name of the monitored quantity.
+        save_weights_only: if True, then only the model's weights will be
+            saved (`model.save_weights(filepath)`), else the full model
+            is saved (`model.save(filepath)`).
+        period: Interval (number of batches) between checkpoints.
+    """
+
+    def __init__(self, filepath, monitor='val_loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1):
+        super(ModelBatchCheckpoint, self).__init__()
+        self.monitor = monitor
+        self.verbose = verbose
+        self.filepath = filepath
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.period = period
+        self.batches_since_last_save = 0
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('ModelCheckpoint mode %s is unknown, '
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.current_epoch = epoch
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+        self.batches_since_last_save += 1
+        if self.batches_since_last_save >= self.period:
+            self.batches_since_last_save = 0
+            filepath = self.filepath.format(epoch=self.current_epoch + 1, batch=batch + 1, **logs)
+            if self.save_best_only:
+                current = logs.get(self.monitor)
+                if current is None:
+                    warnings.warn('Can save best model only with %s available, '
+                                  'skipping.' % (self.monitor), RuntimeWarning)
+                else:
+                    if self.monitor_op(current, self.best):
+                        if self.verbose > 0:
+                            print('\nEpoch %05d / Batch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (self.current_epoch + 1, batch + 1, self.monitor, self.best,
+                                     current, filepath))
+                        self.best = current
+                        if self.save_weights_only:
+                            self.model.save_weights(filepath, overwrite=True)
+                        else:
+                            self.model.save(filepath, overwrite=True)
+                    else:
+                        if self.verbose > 0:
+                            print('\nEpoch %05d / Batch %05d: %s did not improve from %0.5f' %
+                                  (self.current_epoch + 1, batch + 1, self.monitor, self.best))
+            else:
+                if self.verbose > 0:
+                    print('\nEpoch %05d / Batch %05d: saving model to %s' %
+                          (self.current_epoch + 1, batch + 1, filepath))
+                if self.save_weights_only:
+                    self.model.save_weights(filepath, overwrite=True)
+                else:
+                    self.model.save(filepath, overwrite=True)
 
 
 class Model(keras.Model):
