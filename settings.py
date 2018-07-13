8bd10315ef082578d480881cb7f98a32b3c2f86e
Sébastien Lerique <sl@mehho.net>
2018-07-13 16:25:23 +0200

    fix/settings: absolute paths to data folder
    Former-commit-id: defe01582c9ea1839d10dae71d14f8dde1983c19

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
 
-DATA_PATH = 'data'
+PROJECT_ROOT = os.path.split(os.path.abspath(__file__))[0]
+DATA_PATH = os.path.join(PROJECT_ROOT, 'data')
 BEHAVIOUR_PATH = os.path.join(DATA_PATH, 'behaviour')
 SCALE_PATH = os.path.join(DATA_PATH, 'scale')
