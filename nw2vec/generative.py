dbed6b0471b1f866c9de5c46402580a16d146d05
Sébastien Lerique <sl@mehho.net>
2018-10-23 14:31:35 +0200

    feat/stbm: numba-ize stbm generation

--- nw2vec/generative.py
+++ nw2vec/generative.py
@@ -1,41 +1,48 @@
+import numba
 import numpy as np
 
+from nw2vec import utils
 
+
+@numba.jit(nopython=True)
 def stbm(n_nodes=100, n_clusters=40,  # Network parameters
          n_topics=5, n_documents=30, n_slots=140, vocabulary_size=50,  # Language parameters
          ρ=None, π=None, θ=None):
     """Simulate a network + document scenario with STBM."""
 
-    α = np.ones(n_topics)
     β = np.random.random(size=(n_topics, vocabulary_size))
-    β /= np.sum(β, axis=1)[:, np.newaxis]
+    β /= np.expand_dims(np.sum(β, axis=1), -1)
     if ρ is None:
         ρ = np.random.random(n_clusters)
         ρ /= np.sum(ρ)
     if π is None:
         pre_π = np.random.random((n_clusters, n_clusters))
-        π = np.triu(pre_π, k=1) + np.triu(pre_π).T
-    if θ is None:
-        θ = np.random.dirichlet(alpha=α, size=(n_clusters, n_clusters))
-
-    topics_range = range(n_topics)
-    vocabulary_range = range(vocabulary_size)
+        π = np.triu(pre_π, k=1) + np.triu(pre_π, k=0).T
+    # θ cannot be None, but we can't enforce that with Numba, it'll just bail further down
 
     # Order Latent Variables
-    Y = np.random.multinomial(1, pvals=ρ, size=n_nodes)
-    A = np.random.binomial(1, Y @ π @ Y.T)
+    Y = np.random.multinomial(1, pvals=ρ, size=n_nodes).astype(np.float_)
+    A_probs = Y @ π @ Y.T
+    A = np.zeros_like(A_probs)
+    for i in numba.prange(len(A_probs.flat)):
+        A.flat[i] = np.random.binomial(1, A_probs.flat[i])
     Z = np.nan * np.zeros((n_nodes, n_nodes, n_documents, n_slots))
     W = np.nan * np.zeros((n_nodes, n_nodes, n_documents, n_slots))
 
     sources_A, destinations_A = np.where(A)
     for i, j in zip(sources_A, destinations_A):
-        Z[i, j, :, :] = np.random.choice(topics_range,
-                                         p=θ[Y[i].astype(bool), Y[j].astype(bool)][0],
-                                         size=(n_documents, n_slots))
+        qs, rs = np.where(Y[i])[0], np.where(Y[j])[0]
+        assert len(qs) == len(rs) == 1
+        q, r = qs[0], rs[0]
+        topics_choice_aliases = utils.alias_setup(θ[q, r])
+        for k in numba.prange(n_documents * n_slots):
+            Z[i, j].flat[k] = utils.alias_draw(*topics_choice_aliases)
 
-    for k in topics_range:
+    for k in numba.prange(n_topics):
         k_idx = np.where(Z == k)
-        W[k_idx] = np.random.choice(vocabulary_range, p=β[k], size=len(k_idx[0]))
+        vocabulary_choice_aliases = utils.alias_setup(β[k])
+        for idx in zip(*k_idx):
+            W[idx] = utils.alias_draw(*vocabulary_choice_aliases)
 
     return (Y, A, Z, W)
 
