848336bd6f7d2cabc9fdd1ed915134407c494fdf
Sébastien Lerique <sl@mehho.net>
2019-01-29 15:30:04 +0100

    feat/julia: vae working, without feature reconstruction

--- julia/utils.jl
+++ julia/utils.jl
@@ -16,11 +16,11 @@ end
 #finaleltype(::Type{<:AbstractArray{T}}) where {T} = T
 #finaleltype(x::Type{<:TrackedArray{T}}) where {T<:Real} = T
 
-#function scale_center(x; dims = 1)
-#    x = x .- mean(x, dims = dims)
-#    norm = sqrt.(sum(x .^ 2, dims = dims))
-#    x ./ norm
-#end
+function scale_center(x; dims = 1)
+    x = x .- mean(x, dims = dims)
+    norm = sqrt.(sum(x .^ 2, dims = dims))
+    x ./ norm
+end
 
 #function colorbar(x)
 #    a = range(minimum(x), stop = maximum(x), length = 100) |> collect
