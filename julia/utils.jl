68d54acd6ba0fca5669708a16e42ccfa6a41ded7
Sébastien Lerique <sl@mehho.net>
2019-01-30 12:11:39 +0100

    feat/julia: add feature reconstruction

--- julia/utils.jl
+++ julia/utils.jl
@@ -25,6 +25,12 @@ end
 
 logitbinarycrossentropy(logŷ, y; pos_weight = 1) = (1 - y) * logŷ + (1 + (pos_weight - 1) * y) * (log(1 + exp(-abs(logŷ))) + max(-logŷ, 0))
 
+"""Total probability of `y` for the categorical distributions defined by softmax(unormp)"""
+function softmaxcategoricallogprob(unormp, y)
+    shiftedunormp = unormp .- maximum(unormp, dims = 1)
+    sum(y .* (shiftedunormp .- log.(sum(exp.(shiftedunormp), dims = 1))))
+end
+
 adjacency_matrix_diag(g) = adjacency_matrix(g) + Matrix(I, size(g)...)
 
 function markersize(xy)
