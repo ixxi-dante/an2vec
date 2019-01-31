72d2c086143bf5a6e1bb98f08abbb53183117c54
Sébastien Lerique <sl@mehho.net>
2019-01-31 16:28:16 +0100

    feat/julia: clean up Utils

--- julia/utils.jl
+++ julia/utils.jl
@@ -1,9 +1,15 @@
 module Utils
 
 
-export supertypes, finaleltype, scale_center, colorbar
+export mean, scale_center, softmaxcategoricallogprob, logitbinarycrossentropy, adjacency_matrix_diag, randn_like, markersize
 
-using Makie, Statistics, Flux.Tracker, LinearAlgebra, LightGraphs
+using Statistics, Flux.Tracker, LinearAlgebra, LightGraphs
+import Statistics.mean
+
+
+#
+# Introspection helpers
+#
 
 function supertypes(T)
     current = T
@@ -14,8 +20,10 @@ function supertypes(T)
     end
 end
 
-#finaleltype(::Type{<:AbstractArray{T}}) where {T} = T
-#finaleltype(x::Type{<:TrackedArray{T}}) where {T<:Real} = T
+
+#
+# Neural network helpers
+#
 
 function scale_center(x; dims = 1)
     x = x .- mean(x, dims = dims)
@@ -23,15 +31,21 @@ function scale_center(x; dims = 1)
     x ./ norm
 end
 
-logitbinarycrossentropy(logŷ, y; pos_weight = 1) = (1 - y) * logŷ + (1 + (pos_weight - 1) * y) * (log(1 + exp(-abs(logŷ))) + max(-logŷ, 0))
-
 """Total probability of `y` for the categorical distributions defined by softmax(unormp)"""
 function softmaxcategoricallogprob(unormp, y)
     shiftedunormp = unormp .- maximum(unormp, dims = 1)
     sum(y .* (shiftedunormp .- log.(sum(exp.(shiftedunormp), dims = 1))))
 end
 
+logitbinarycrossentropy(logŷ, y; pos_weight = 1) = (1 - y) * logŷ + (1 + (pos_weight - 1) * y) * (log(1 + exp(-abs(logŷ))) + max(-logŷ, 0))
 adjacency_matrix_diag(g) = adjacency_matrix(g) + Matrix(I, size(g)...)
+randn_like(target::A) where A<:AbstractArray{T} where T = randn(T, size(target))
+mean(a::AbstractArray...) = sum(a) / length(a)
+
+
+#
+# Plotting helpers
+#
 
 function markersize(xy)
     (xmin, xmax), (ymin, ymax) = extrema(xy, dims = 2)
@@ -39,11 +53,6 @@ function markersize(xy)
 end
 
 markersize(xy::TrackedArray) = markersize(xy.data)
-
-#function colorbar(x)
-#    a = range(minimum(x), stop = maximum(x), length = 100) |> collect
-#    heatmap(reshape(a, 1, :))
-#end
 
 
 end
