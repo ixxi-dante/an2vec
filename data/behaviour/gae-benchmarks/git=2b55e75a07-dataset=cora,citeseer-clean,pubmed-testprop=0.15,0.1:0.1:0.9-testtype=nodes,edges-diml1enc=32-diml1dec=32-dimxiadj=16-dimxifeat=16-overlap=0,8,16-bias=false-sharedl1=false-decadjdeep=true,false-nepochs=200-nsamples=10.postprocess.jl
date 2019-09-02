eb6abcb6a852f0a44f78c26acc58cf41144952e0
Sébastien Lerique <sl@mehho.net>
2019-09-02 15:48:52 +0200

    feat/data: add exploration of different dimxi and different training sizes

---
+++ data/behaviour/gae-benchmarks/git=2b55e75a07-dataset=cora,citeseer-clean,pubmed-testprop=0.15,0.1:0.1:0.9-testtype=nodes,edges-diml1enc=32-diml1dec=32-dimxiadj=16-dimxifeat=16-overlap=0,8,16-bias=false-sharedl1=false-decadjdeep=true,false-nepochs=200-nsamples=10.postprocess.jl
@@ -0,0 +1,74 @@
+using DataFrames, NPZ, GZip, CSV, ProgressMeter
+
+
+folder = "git=2b55e75a07-dataset=cora,citeseer-clean,pubmed-testprop=0.15,0.1:0.1:0.9-testtype=nodes,edges-diml1enc=32-diml1dec=32-dimxiadj=16-dimxifeat=16-overlap=0,8,16-bias=false-sharedl1=false-decadjdeep=true,false-nepochs=200-nsamples=10"
+pattern = Regex(folder * "/dataset=([^-]+(?:-clean)?)-testtype=([^-]+)-testprop=([^-]+)-decadjdeep=([^-]+)-dimxi=([^-]+)-sample=([^-]+).npz")
+basedir = "data/behaviour/gae-benchmarks/"
+refnfiles = 3600
+
+for (root, dirs, files) in walkdir(basedir)
+    println("root = $root")
+
+    npzfiles = filter((f) -> f[end-2:end] == "npz", files)
+    nfiles = length(npzfiles)
+    println("$nfiles files")
+    if nfiles != refnfiles
+        continue
+    end
+
+    outpath = "$root.csv.gz"
+    if stat(outpath).size > 0
+        println("$outpath already exists")
+        continue
+    end
+
+    df = Array{Dict}(undef, nfiles)
+    nepochs = nothing
+    @showprogress for (i, file) in enumerate(npzfiles)
+        path = joinpath(root, file)
+        m = match(pattern, path)
+        if m === nothing
+            println("No match: $path")
+            continue
+        end
+
+        history = npzread(path)
+        r = Dict()
+        for (k, v) in history
+            if nepochs === nothing
+                nepochs = length(v)
+            else
+                @assert nepochs == length(v)
+            end
+            r[k] = v
+        end
+
+        dataset, testtype, testprop, decadjdeep, dimξ, sample = m[1], m[2], parse(Float32, m[3]), parse(Bool, m[4]), parse(Int, m[5]), parse(Int, m[6])
+        r["epoch"] = 1:nepochs
+        r["dataset"] = repeat([dataset], nepochs)
+        r["testtype"] = repeat([testtype], nepochs)
+        r["testprop"] = repeat([testprop], nepochs)
+        r["decadjdeep"] = repeat([decadjdeep], nepochs)
+        r["dimξ"] = repeat([dimξ], nepochs)
+        r["sample"] = repeat([sample], nepochs)
+
+        df[i] = r
+    end
+
+    println("equalizing dicts")
+    cols = union(Set.(keys.(df))...)
+    for r in df
+        for col in cols
+            if !haskey(r, col)
+                r[col] = repeat([0f0], nepochs)
+            end
+        end
+    end
+
+    println("saving to $outpath")
+    df = vcat(DataFrame.(df)...)
+    GZip.open(outpath, "w") do io
+        CSV.write(io, df)
+    end
+
+end
---
+++ data/behaviour/gae-benchmarks/git=2b55e75a07-dataset=cora,citeseer-clean-testprop=0.15,0.1:0.1:0.9-testtype=nodes,edges-diml1enc=32-diml1dec=32-dimxiadj=16-dimxifeat=16-overlap=0,8,16-bias=false-sharedl1=false-decadjdeep=true,false-nepochs=200-nsamples=10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+. $HOME/anaconda3/etc/profile.d/conda.sh
+conda activate base36-gpu
+date
+PATH=$PATH:~/bin OMP_NUM_THREADS=1 JULIA_NUM_THREADS=1 parallel \
+        --sshloginfile parallel-slaves --sshdelay 0.1 \
+        --bar --header : \
+        --env PATH --env JULIA_NUM_THREADS --env OMP_NUM_THREADS \
+        'cd ~/Code/nw2vec && OUTFOLDER=data/behaviour/gae-benchmarks/git=2b55e75a07-dataset=cora,citeseer-clean,pubmed-testprop=0.15,0.1:0.1:0.9-testtype=nodes,edges-diml1enc=32-diml1dec=32-dimxiadj=16-dimxifeat=16-overlap=0,8,16-bias=false-sharedl1=false-decadjdeep=true,false-nepochs=200 && OUTPATH=$OUTFOLDER/dataset={dataset}-testtype={tt}-testprop={tp}-decadjdeep={decadjdeep}-dimxi={dimxi}-sample={sample}.npz && mkdir -p $OUTFOLDER && if test -e $OUTPATH; then echo "Already done"; false; fi && nice julia julia/an2vec-dataset.jl --dataset datasets/gae-benchmarks/{dataset}.npz --testtype {tt} --testprop {tp} --label-distribution {ld} --diml1enc 32 --diml1dec 32 --dimxiadj 16 --dimxifeat 16 --overlap $(( 32 - {dimxi} )) --nepochs 200 --bias false --sharedl1 false --decadjdeep {decadjdeep} --savehistory $OUTPATH' \
+        ::: decadjdeep true false \
+        ::: dataset cora citeseer-clean :::+ ld bernoulli bernoulli \
+        ::: tt nodes edges \
+        ::: tp 0.15 $(seq 0.1 0.1 0.9) \
+        ::: dimxi $(seq 32 -8 16) \
+        ::: sample $(seq 1 10)
