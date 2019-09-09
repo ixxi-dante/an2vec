d37aad35948fe94640f26826c229c2ede159f9bf
Sébastien Lerique <sl@mehho.net>
2019-09-09 10:21:01 +0200

    chore/data: merge graphsage result files

--- data/behaviour/gae-benchmarks/graphsage-git=f31b6c5296-dataset=cora,citeseer-clean,pubmed-testtype=nodes,edges-testprop=0.15,0.1:0.1:0.9-diml1enc=32-dimxiadj=16-bias=false-nepochs=4-nsamples=10.sh
+++ data/behaviour/gae-benchmarks/graphsage-git=f31b6c5296-dataset=cora,citeseer-clean,pubmed-testtype=nodes,edges-testprop=0.15,0.1:0.1:0.9-diml1enc=32-dimxiadj=16-bias=false-nepochs=4-nsamples=10.sh
@@ -7,10 +7,10 @@ date
 PATH=$PATH:~/bin OMP_NUM_THREADS=12 JULIA_NUM_THREADS=12 parallel \
         --bar --header : \
         -j6 \
-        --results data/behaviour/gae-benchmarks/graphsage-git=f31b6c5296-dataset=cora,citeseer-clean-testtype=edges-testprop=0.15,0.1:0.1:0.9-diml1enc=32-dimxiadj=16-bias=false-nepochs=4-nsamples=10.csv \
+        --results data/behaviour/gae-benchmarks/graphsage-git=f31b6c5296-dataset=cora,citeseer-clean,pubmed-testtype=nodes,edges-testprop=0.15,0.1:0.1:0.9-diml1enc=32-dimxiadj=16-bias=false-nepochs=4-nsamples=10.csv \
         --env PATH --env JULIA_NUM_THREADS --env OMP_NUM_THREADS \
         'cd ~/Code/nw2vec && python projects/behaviour/gae-benchmarks-graphsage.py --dataset datasets/gae-benchmarks/{dataset}.npz --testtype {tt} --testprop {tp} --diml1enc 32 --dimxiadj 16 --nepochs 4 --bias False --nworkers 1' \
         ::: sample $(seq 1 10) \
-        ::: dataset cora citeseer-clean \
-        ::: tt edges \
+        ::: dataset cora citeseer-clean pubmed \
+        ::: tt nodes edges \
         ::: tp 0.15 $(seq 0.1 0.1 0.9)
