c5ca22d84135789dac5e799a77ba795e577ac963
Sébastien Lerique <sl@eauchat.org>
2019-11-07 10:27:40 +0100

    fix/twitter: also save model weights

--- data/twitter/git=679a9eb593-an2vec-diml1enc=32-diml1dec=32-dimxiadj=16-dimxifeat=16-overlap=0,8,16-bias=false-sharedl1=false-decadjdeep=true-nepochs=200.sh
+++ data/twitter/git=679a9eb593-an2vec-diml1enc=32-diml1dec=32-dimxiadj=16-dimxifeat=16-overlap=0,8,16-bias=false-sharedl1=false-decadjdeep=true-nepochs=200.sh
@@ -8,7 +8,7 @@ PATH=$PATH:~/bin OMP_NUM_THREADS=8 JULIA_NUM_THREADS=8 parallel \
         --bar --header : \
         -j5 \
         --env PATH --env JULIA_NUM_THREADS --env OMP_NUM_THREADS --env OUTFOLDER \
-        'cd ~/Code/nw2vec && OUTPATH=$OUTFOLDER/dataset={dataset}-nclusters={nclusters}-ld={ld}-dimxi={dimxi}.npz && mkdir -p $OUTFOLDER && if test -e $OUTPATH; then echo "Already done"; false; fi && julia julia/an2vec-dataset.jl --dataset data/twitter/git=679a9eb593-csv_to_npz-mt=5-tmw=3-w2v_dim=50-w2v_iter=10-cho=True-nclusters=10,20,50,80,100/dataset={dataset}-nclusters={nclusters}.npz --label-distribution {ld} --diml1enc 32 --diml1dec 32 --dimxiadj 16 --dimxifeat 16 --overlap $(( 32 - {dimxi} )) --nepochs 200 --bias false --sharedl1 false --decadjdeep {decadjdeep} --savehistory $OUTPATH' \
+        'cd ~/Code/nw2vec && OUTBASE=$OUTFOLDER/dataset={dataset}-nclusters={nclusters}-ld={ld}-dimxi={dimxi} && OUTHISTORY=$OUTBASE-history.npz && OUTWEIGHTS=$OUTBASE-weights.bson && mkdir -p $OUTFOLDER && if test -e $OUTWEIGHTS; then echo "Weights already trained"; false; fi && julia julia/an2vec-dataset.jl --dataset data/twitter/git=679a9eb593-csv_to_npz-mt=5-tmw=3-w2v_dim=50-w2v_iter=10-cho=True-nclusters=10,20,50,80,100/dataset={dataset}-nclusters={nclusters}.npz --label-distribution {ld} --diml1enc 32 --diml1dec 32 --dimxiadj 16 --dimxifeat 16 --overlap $(( 32 - {dimxi} )) --nepochs 200 --bias false --sharedl1 false --decadjdeep {decadjdeep} --savehistory $OUTHISTORY --saveweights $OUTWEIGHTS' \
         ::: decadjdeep true \
         ::: dataset retweetsrange retweetsrange :::+ ld bernoulli normal \
         ::: nclusters 10 20 50 80 100 \
